c89f63a0195dbdc24b348b6d77f76b4fa668cb45
karosaxy <araromirichard@gmail.com>
2022-09-19 18:21:54 +0100

    my first commit, route refresher

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./app.css">
+    <title>My Blog</title>
+</head>
+
+<body>
+    <article>
+
+        <h2><a href="/post">My First Post</a></h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore cumque provident neque cum aperiam repellat
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet ad enim odio unde saepe! Similique, eius fugiat velit unde praesentium corrupti tempora aliquid dicta at quidem. Tempora eveniet, eum autem maxime accusamus obcaecati ad, quibusdam cum earum quia unde neque reiciendis hic nihil at. Deleniti expedita consequatur aliquam eveniet eaque!
+            ipsam
+            tenetur! Tempora sint fugiat, esse facilis aspernatur amet. Quam, maxime. Neque amet iure officia sed
+            voluptatibus
+            dignissimos non commodi, vero obcaecati veniam autem perspiciatis pariatur nostrum est eligendi dolorum
+            ipsam at
+            accusantium ullam cupiditate! Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias nulla
+            distinctio fuga molestias possimus omnis culpa repellendus quaerat cum ipsum sunt ipsam, totam quam officiis
+            enim reprehenderit ullam, at, earum dolor. Dolore amet totam iure, dolorem ullam repellat perferendis
+            architecto delectus optio possimus, veniam esse, at adipisci in suscipit ipsam.</p>
+    </article>
+    <article>
+
+        <h2><a href="/post">My Second Post</a></h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore cumque provident neque cum aperiam repellat
+            ipsam
+            tenetur! Tempora sint fugiat, esse facilis aspernatur amet. Quam, maxime. Neque amet iure officia sed
+            voluptatibus
+            dignissimos non commodi, vero obcaecati veniam autem perspiciatis pariatur nostrum est eligendi dolorum
+            ipsam at
+            accusantium ullam cupiditate! Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias nulla
+            distinctio fuga molestias possimus omnis culpa repellendus quaerat cum ipsum sunt ipsam, totam quam officiis
+            enim reprehenderit ullam, at, earum dolor. Dolore amet totam iure, dolorem ullam repellat perferendis
+            architecto delectus optio possimus, veniam esse, at adipisci in suscipit ipsam.</p>
+    </article>
+    <article>
+
+        <h2><a href="/post">My Third Post</a></h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore cumque provident neque cum aperiam repellat
+            ipsam
+            tenetur! Tempora sint fugiat, esse facilis aspernatur amet. Quam, maxime. Neque amet iure officia sed
+            voluptatibus
+            dignissimos non commodi, vero obcaecati veniam autem perspiciatis pariatur nostrum est eligendi dolorum
+            ipsam at
+            accusantium ullam cupiditate! Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias nulla
+            distinctio fuga molestias possimus omnis culpa repellendus quaerat cum ipsum sunt ipsam, totam quam officiis
+            enim reprehenderit ullam, at, earum dolor. Dolore amet totam iure, dolorem ullam repellat perferendis
+            architecto delectus optio possimus, veniam esse, at adipisci in suscipit ipsam.</p>
+    </article>
+
+
+</body>
+
+</html>
