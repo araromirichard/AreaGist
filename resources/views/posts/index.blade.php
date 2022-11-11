5a48e4d6b0bd0b6f5701efda1364a31fe9bb996e
karosaxy <araromirichard@gmail.com>
2022-11-11 18:48:41 +0100

    pagination lesson

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,13 +1,16 @@
 <x-layout>
-    @include('posts._header')
+  @include('posts._header')
 
-    <main class="max-w-7xl mx-auto mt-6 lg:mt-20 space-y-6">
-        @if($posts->count())
-      <x-posts-grid :posts="$posts" />
-        @else
+  <main class="max-w-7xl mx-auto mt-6 lg:mt-20 space-y-6">
+    @if($posts->count())
+    <x-posts-grid :posts="$posts" />
 
-        <p class="text-center">There are no posts for now, Please Kindly check Back later...</p>
-        @endif
-    </main>
+    {{ $posts->links() }}
 
-</x-layout> 
+    @else
+
+    <p class="text-center">There are no posts for now, Please Kindly check Back later...</p>
+    @endif
+  </main>
+
+</x-layout>
